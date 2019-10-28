c6c70e0a8bb3baa6fdcc9a82e17e330d16949162
Sagar Sidhu <sagarsidhu002@gmail.com>
2019-10-28 01:03:03 -0700

    Lab 4 Complete

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -8,104 +8,106 @@ function showForm() {
   }
 
 }
-function loadData() {
-  if (localStorage.getItem != null) {
-    var data = localStorage.getItem('items');
-    var dataSplit = JSON.parse(data);
-    for (var i = 0; i < dataSplit.length/3; i++) {
-      var imageURL = dataSplit[i];
-      var artistName = dataSplit[i + 1];
-      var aboutArtist = dataSplit[i + 2];
+var n = localStorage.getItem('on_load_counter');
+if (n === null) {
+  n = 0;
+}
 
+var storedData = localStorage.getItem("artists");
+if (storedData) {
+  ArrayData = JSON.parse(storedData);
+}
+function loadData() {
+  //localStorage.clear();
+
+  if (n !== null) {
+    var arrayLength = ArrayData.length - 1;
+    for(var i = 0; i < arrayLength; i++) {
       var div = document.createElement('div');
       div.setAttribute('class', 'content');
-      div.setAttribute('id', counter);
+      div.setAttribute('id', ArrayData[i+3]);
       document.body.appendChild(div);
-
+    
       var x = document.createElement("IMG");
-      x.setAttribute("src", imageURL);
-      x.setAttribute('class', counter);
+      x.setAttribute("src", ArrayData[i+2]);
+      x.setAttribute('class', n);
       div.appendChild(x);
-      saveData(imageURL);
-
+    
       var div2 = document.createElement('div');
       div2.setAttribute('class', 'artistName');
-      div2.textContent = artistName;
+      div2.textContent = ArrayData[i];
       div.appendChild(div2);
-      saveData(artistName);
-
+    
       var div3 = document.createElement('div');
       div3.setAttribute('class', 'artistSchool');
-      div3.textContent = aboutArtist;
+      div3.textContent = ArrayData[i+1];
       div2.appendChild(div3);
-      saveData(aboutArtist);
-
+    
       var btn = document.createElement("button");
       btn.setAttribute('class', 'deleteBtn');
-      btn.setAttribute('onclick', 'deleteDiv(' + counter + ',\'' + imageURL + '\' )');
+      btn.setAttribute('onclick', 'deleteDiv(' + ArrayData[i+3] + ')');
       btn.innerHTML = 'Delete';
       div3.appendChild(btn);
-
-      document.getElementById("myForm").style.display = "none";
-      document.getElementById("btn1").disabled = false;
-
-      document.getElementById("myForm").reset();
-
-      counter+3;
+      i+=3;
     }
   }
 }
-var artistArray = [];
-var count = 0;
-function saveData(toSave) {
-  artistArray[count] = toSave;
-  count++;
-  localStorage.setItem('items', JSON.stringify(artistArray))
-}
-function deleteArrayItem(toDel) {
-  var z = toDel;
-  console.log("z", z);
-  for (index = 0; index < artistArray.length; index++) {
-    if (artistArray[index] == z) {
-      artistArray.splice(index, index + 3);
+var artistArray = []
+
+function addToArray(artistName) {
+  artistArray.push(artistName);
+  n++;
+  if (n === 1) {
+    var x = artistArray;
+  } else {
+    if (n === 2) {
+      var storedData = localStorage.getItem("artists");
+      if (storedData) {
+        ArrayData = JSON.parse(storedData);
+        var x = ArrayData.concat(artistArray);
+        x.pop;
+      }
     }
+    var x = artistArray.concat(ArrayData);
   }
-  localStorage.setItem('items', JSON.stringify(artistArray))
-
-
+  localStorage.setItem("artists", JSON.stringify(x));
+  localStorage.setItem("on_load_counter", n);
 }
+
 var counter = 0;
 function addToDirectory() {
   var artistName = document.getElementById("artistName").value;
   var aboutArtist = document.getElementById("aboutArist").value;
   var imageURL = document.getElementById("imgURL").value;
 
+  addToArray(artistName);
+  addToArray(aboutArtist);
+  addToArray(imageURL);
+  addToArray(n / 4);
+
   var div = document.createElement('div');
   div.setAttribute('class', 'content');
-  div.setAttribute('id', counter);
+  div.setAttribute('id', n / 4);
   document.body.appendChild(div);
 
   var x = document.createElement("IMG");
   x.setAttribute("src", imageURL);
-  x.setAttribute('class', counter);
+  x.setAttribute('class', n);
   div.appendChild(x);
-  saveData(imageURL);
 
   var div2 = document.createElement('div');
   div2.setAttribute('class', 'artistName');
   div2.textContent = artistName;
   div.appendChild(div2);
-  saveData(artistName);
 
   var div3 = document.createElement('div');
   div3.setAttribute('class', 'artistSchool');
   div3.textContent = aboutArtist;
   div2.appendChild(div3);
-  saveData(aboutArtist);
 
   var btn = document.createElement("button");
   btn.setAttribute('class', 'deleteBtn');
-  btn.setAttribute('onclick', 'deleteDiv(' + counter + ',\'' + imageURL + '\' )');
+  btn.setAttribute('onclick', 'deleteDiv(' + n/4 + ')');
   btn.innerHTML = 'Delete';
   div3.appendChild(btn);
 
@@ -116,11 +118,22 @@ function addToDirectory() {
 
   counter++;
 }
-function deleteDiv(todel, URLtoDel) {
+function deleteDiv(todel) {
   var z = document.getElementById(todel);
   z.style.display = "none";
-  deleteArrayItem(URLtoDel);
-
+  delFromArray(todel);
+}
+function delFromArray(artistName) {
+  console.log("Before", ArrayData);
+  for(var i = 0; i < ArrayData.length; i++) {
+    if(ArrayData[i] === artistName) {
+      var n = i;
+    }
+  }
+  var spliceVar = n - 3;
+  ArrayData.splice(spliceVar, 4);
+  localStorage.setItem("artists", JSON.stringify(ArrayData));
+  console.log(ArrayData);
 }
 function search() {
   var input = document.getElementById("search");
